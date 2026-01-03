3b7c1cb90231b943871c2609e5820accb8ed030e
Quinn Michaels <quinn@indra.ai>
2026-01-02 20:34:58 -0800

    Update to latest deva core

--- index.js
+++ index.js
@@ -1,9 +1,9 @@
 "use strict";
+// Indu Deva
 // Copyright ©2000-2025 Quinn A Michaels; All rights reserved. 
 // Legal Signature Required For Lawful Use.
 // Distributed under VLA:31691656237827831406 LICENSE.md
 // Wednesday, November 26, 2025 - 6:10:27 AM
-// Indu Deva
 
 import Deva from '@indra.ai/deva';
 import pkg from './package.json' with {type:'json'};
